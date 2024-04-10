c14e93497ce0575070ecf6cff96468a2fa3a4f43
Phuree <phureeppp@gmail.com>
2024-04-10 21:40:42 +0700

    change UI & input validation

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -7,8 +7,23 @@ import deleteBooking from "@/libs/deleteBooking";
 import updateBooking from "@/libs/updateBooking";
 import { useRouter } from "next/navigation";
 import { FaInfoCircle } from "react-icons/fa";
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from "dayjs";
+import DateReserve from "./DateReserve";
+import { DatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import Image from "next/image";
 
+const cars: Car[] = [
+  { model: "Honda Civic", img: "/cars/civic.jpg", description: "Cool car" },
+  { model: "Honda Accord", img: "/cars/accord.jpg", description: "Cool car" },
+  {
+    model: "Toyota Fortuner",
+    img: "/cars/fortuner.jpg",
+    description: "Cool car",
+  },
+  { model: "Tesla Model 3", img: "/cars/tesla.jpg", description: "Cool car" },
+];
 
 export default function BookingList({
   bookings,
@@ -32,12 +47,16 @@ export default function BookingList({
       tel: "",
     },
     createdAt: "",
+    discountPoint: 0,
+    cost: 0,
+    addedPoint: 0,
   });
 
   const [editID, setEditID] = useState("");
   const router = useRouter();
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e.target) return;
     const { name, value } = e.target;
     setForm({
       ...form,
@@ -71,6 +90,9 @@ export default function BookingList({
         tel: "",
       },
       createdAt: "",
+      discountPoint: 0,
+      cost: 0,
+      addedPoint: 0,
     });
   }
 
@@ -109,7 +131,7 @@ export default function BookingList({
           </header>
           <div className="w-[80vw] mt-4 mx-auto overflow-auto rounded-lg shadow">
             <table className="table table-zebra">
-              <thead >
+              <thead>
                 <tr className="">
                   <th className="text-sm">No.</th>
                   <th className="text-sm">User</th>
@@ -120,93 +142,128 @@ export default function BookingList({
                   <th className="text-sm">Cost</th>
                 </tr>
               </thead>
-              <tbody className="">
+              <tbody>
                 {bookings.data.map((item: Booking, idx) => (
-                  <tr className="" key={idx}>
-                    <th>{idx + 1}</th>
+                  <tr key={idx}>
+                    <th className="">{idx + 1}</th>
                     {editID === item._id ? (
-                      <div className="flex flex-col">
-                        <label>Car</label>
-                        <input
-                          value={form.car !== "" ? form.car : item.car}
-                          onChange={(e) => handleChange(e)}
-                          type="text"
-                          placeholder="car..."
-                          name="car"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>bookingDate</label>
-                        <input
-                          value={
-                            form.bookingDate !== ""
-                              ? form.bookingDate
-                              : item.bookingDate
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="text"
-                          placeholder="bookingDate..."
-                          name="bookingDate"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>daySpend</label>
-                        <input min={1}
-                          value={
-                            form.daySpend !== 0 ? form.daySpend : item.daySpend
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="number"
-                          placeholder="daySpend..."
-                          name="daySpend"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>createdAt</label>
-                        <input
-                          value={
-                            form.createdAt !== ""
-                              ? form.createdAt
-                              : item.createdAt
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="text"
-                          placeholder="createdAt..."
-                          name="createdAt"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                        <label>Provider</label>
-                        <input
-                          value={
-                            form.rentalProvider.name !== ""
-                              ? form.rentalProvider.name
-                              : item.rentalProvider.name
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="text"
-                          placeholder="Provider..."
-                          name="Provider"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500 bg-black text-white"
-                        />
-                        <label>Cost</label>
-                        <input min={0}
-                          value={
-                            form.rentalProvider.cost !== 0
-                              ? form.rentalProvider.cost
-                              : item.rentalProvider.cost
-                          }
-                          onChange={(e) => handleChange(e)}
-                          type="number"
-                          placeholder="cost..."
-                          name="cost"
-                          className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500  bg-black text-white"
-                        />
+                      <div className="">
+                        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                          <figure>
+                            <img
+                              src={
+                                cars.filter((c) => c.model === form.car).at(0)
+                                  ?.img
+                              }
+                              alt={form.car}
+                            />
+                          </figure>
+                          <div className="card-body">
+                            <label>Car</label>
+                            <select
+                              name="car"
+                              className="select select-info w-full max-w-xs"
+                              onChange={(e: any) => {
+                                handleChange(e);
+                              }}
+                            >
+                              <option disabled selected>
+                                {form.car}
+                              </option>
+                              {cars.map((car: any) => (
+                                <option key={car.model} value={car.model}>
+                                  {car.model}
+                                </option>
+                              ))}
+                            </select>
+                            <label>bookingDate</label>
+
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                              <DatePicker
+                                name="bookingDate"
+                                value={dayjs(form.bookingDate)}
+                                className="rounded-lg bg-white"
+                                format="DD/MM/YYYY"
+                                onChange={(e) => {
+                                  if (!e) return;
+                                  setForm({
+                                    ...form,
+                                    bookingDate: e?.toString(),
+                                  });
+                                }}
+                              />
+                            </LocalizationProvider>
+
+                            <label>daySpend</label>
+                            <input
+                              min={1}
+                              value={
+                                form.daySpend !== 0
+                                  ? form.daySpend
+                                  : item.daySpend
+                              }
+                              onChange={(e) => {
+                                if (+e.target.value < 1 && e.target.value)
+                                  return;
+                                e.target.value = Math.floor(
+                                  +e.target.value
+                                ).toString();
+                                handleChange(e);
+                              }}
+                              type="number"
+                              placeholder="daySpend..."
+                              name="daySpend"
+                              className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500"
+                            />
+                            <label>Provider</label>
+                            <input
+                              value={
+                                form.rentalProvider.name !== ""
+                                  ? form.rentalProvider.name
+                                  : item.rentalProvider.name
+                              }
+                              onChange={(e) => handleChange(e)}
+                              type="text"
+                              placeholder="Provider..."
+                              name="Provider"
+                              className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500 bg-black text-white"
+                            />
+                            <label>Cost</label>
+                            <input
+                              min={0}
+                              value={
+                                form.rentalProvider.cost !== 0
+                                  ? form.rentalProvider.cost
+                                  : item.rentalProvider.cost
+                              }
+                              onChange={(e) => handleChange(e)}
+                              type="number"
+                              placeholder="cost..."
+                              name="cost"
+                              className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500  bg-black text-white"
+                            />
+                            <label>Total Cost (after discount)</label>
+                            <input
+                              value={item.cost}
+                              className="px-4 py-2 border-2 rounded-md focus:outline-none focus:border-blue-500  bg-black text-white"
+                            />
+                          </div>
+                        </div>
                       </div>
                     ) : (
                       <>
                         <td className="whitespace-nowrap">{item.user}</td>
                         <td className="whitespace-nowrap">{item.car}</td>
-                        <td className="whitespace-nowrap">{  dayjs(item.bookingDate).format('DD/MM/YYYY')  }</td>
+                        <td className="whitespace-nowrap">
+                          {dayjs(item.bookingDate).format("DD/MM/YYYY")}
+                        </td>
                         <td className="whitespace-nowrap">{item.daySpend}</td>
-                        <td className="whitespace-nowrap">{item.rentalProvider.name}</td>
-                        <td className="whitespace-nowrap">{item.rentalProvider.cost}</td>
+                        <td className="whitespace-nowrap">
+                          {item.rentalProvider.name}
+                        </td>
+                        <td className="whitespace-nowrap">
+                          {item.rentalProvider.cost}
+                        </td>
                       </>
                     )}
 
